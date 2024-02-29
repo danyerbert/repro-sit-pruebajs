7512ee2964411b82855a5a88c60bc2547038ebe4
danyerbert <danyerbert@gmail.com>
2024-02-29 07:09:53 -0400

    Eliminar Dispositivo

--- dispositivosentrada.php
+++ dispositivosentrada.php
@@ -42,7 +42,7 @@ $result = $mysqli->query($sql3);
 
 //Consulta para traer los datos almacenados de los dispositivos
 
-$sql2 = "SELECT d.ic_dispositivo, d.serial_equipo, d.serial_de_cargador, d.fecha_de_recepcion, d.estado_recepcion_equipo,d.fecha_de_entrega, d.observaciones_analista, d.id_datos_del_beneficiario, d.id_origen, j.nombre, j.modelo, k.origen, m.estatus, b.tipo_de_motivo , t.estado FROM datos_del_dispotivo AS d 
+$sql2 = "SELECT d.id_dispositivo,d.ic_dispositivo, d.serial_equipo, d.serial_de_cargador, d.fecha_de_recepcion, d.estado_recepcion_equipo,d.fecha_de_entrega, d.observaciones_analista, d.id_datos_del_beneficiario, d.id_origen, j.nombre, j.modelo, k.origen, m.estatus, b.tipo_de_motivo , t.estado FROM datos_del_dispotivo AS d 
 INNER JOIN tipo_de_equipo AS j ON j.id_tipo_de_equipo=d.id_tipo_de_dispositivo
 INNER JOIN origen AS k ON k.id_origen = d.id_origen
 INNER JOIN estatus AS m ON m.id_estatus = d.id_estatus
@@ -227,7 +227,7 @@ $resultadoResponsable = $mysqli->query($sqlResponsable);
                                                     width="15" height="15">
                                                 Editar</a>
                                                 <a class="dropdown-item btn btn-danger"
-                                                    href="eliminardispositivo.php?id='.$row['ic_dispositivo'].'"><img
+                                                    href="php/eliminar/eliminarDispositivo.php?id='.$row['id_dispositivo'].'"><img
                                                     src="img/svg/eliminar.svg " alt="Industrias Canaima" width="15"
                                                     height="15"> Eliminar</a>
                          ';
---
+++ php/eliminar/eliminarDispositivo.php
@@ -0,0 +1,62 @@
+<?php
+require "../../config/conexion.php";
+session_start();
+if (!isset($_SESSION['id_usuarios'])) {
+	header("Location:index.php");
+}else{
+    if ($_SESSION['id_roles'] != 1) {
+        header("Location: index.php");
+    }
+}
+
+//obtener el ID del beneficiario a eliminar
+$idDispositivo = $_REQUEST['id'];
+//Realizamos la consulta para eliminar el benificiario
+$sql = "UPDATE datos_del_dispotivo SET descontinuado = 1 WHERE id_dispositivo = '$idDispositivo'";
+
+// Obtenemos la respuesta de esa consulta
+$respuesta = $mysqli->query($sql);
+
+// Comprobamos si la respuesta se realizo o no y mostramos su respectivo mensaje
+if ($respuesta) {
+    echo "
+    <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
+    <script language='JavaScript'>
+    document.addEventListener('DOMContentLoaded', function() {
+        Swal.fire({
+            icon: 'success',
+            title: 'El dispositivo fue eliminado correctamente.',
+            showCancelButton: false,
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'OK'
+          }).then(() => {
+
+            location.assign('../../dispositivosentrada.php');
+
+          });
+});
+    </script>";
+}else {
+     echo "
+    <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
+    <script language='JavaScript'>
+    document.addEventListener('DOMContentLoaded', function() {
+        Swal.fire({
+            icon: 'error',
+            title: 'Algo salio mal. Intenta de nuevo',
+            showCancelButton: false,
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'OK'
+          }).then(() => {
+
+            location.assign('../../dispositivosentrada.php');
+
+         });
+});
+    </script>";
+}
+
+
+
+
+?>
